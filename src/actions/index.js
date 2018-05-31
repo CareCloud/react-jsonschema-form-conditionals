31cdcdefe6b10813785e27118b40c33b22f01d09
Juan Norris <jnorris@carecloud.com>
2018-05-31 17:17:16 -0300

    Support form data change and other stuff.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,29 @@
 import remove from "./remove";
 import require from "./require";
+import unRequire from "./unRequire";
 import uiAppend from "./uiAppend";
 import uiReplace from "./uiReplace";
 import uiOverride from "./uiOverride";
+import formData from "./formData";
 
 export const DEFAULT_ACTIONS = {
   remove,
   require,
+  unRequire,
   uiAppend,
   uiReplace,
   uiOverride,
+  formData,
 };
 
-export default function execute(
+export default async function execute(
   { type, params },
   schema,
   uiSchema,
   formData,
-  extraActions = {}
+  extraActions = {},
+  prevFormData
 ) {
   let action = extraActions[type] ? extraActions[type] : DEFAULT_ACTIONS[type];
-  action(params, schema, uiSchema, formData);
+  await action(params, schema, uiSchema, formData, prevFormData);
 }
